ed31943137816e59a70cee7c400afb3f00a97971
Eithan Refaely <eithanrefaely@gmail.com>
2021-10-22 06:42:46 +0300

    Update usersHandler.js

--- back/src/usersHandler.js
+++ back/src/usersHandler.js
@@ -7,7 +7,7 @@ module.exports.usersHandler = (req, res, next) => {
        throw { status: 401, message: "username header is missing, example for header: username: Eithan"}
    }
 
-   const userPath = path.resolve(path.join("C:/Users/eitha/Documents/GitHub/secondWeekendTask/secondWeekendPokemon","/users", username));
+   const userPath = path.resolve(path.join(__dirname, "/users", username));
    if(!fs.existsSync(userPath)) {
        fs.mkdir(userPath, { recursive: true }, (err) => {
         if (err) throw err;
